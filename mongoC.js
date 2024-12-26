ad645bae24ea7f45a2a64483231ddcdc4b27ca4b
sailesh <sksailesh789@gmail.com>
2024-12-26 12:35:23 +0545

    deploy to ec2

--- mongoC.js
+++ mongoC.js
@@ -1,13 +1,13 @@
 import { MongoClient } from "mongodb";
 
 const password = encodeURIComponent(process.env.MONGO_PASSWORD.trim());
-const connectionString = `mongodb+srv://integrationninjas:${password}@devcluster.xf2gcci.mongodb.net/?retryWrites=true&w=majority`; // clustore url
+const connectionString = `mongodb+srv://sailesh:${password}@cluster0.p0u23.mongodb.net/?retryWrites=true&w=majority`; // clustore url
 const client = new MongoClient(connectionString);
 let conn;
 try {
   conn = await client.connect();
-  console.log("connection successful")
-} catch(e) {
+  console.log("connection successful");
+} catch (e) {
   console.error(e);
 }
 let db = conn.db("integration_ninjas");
